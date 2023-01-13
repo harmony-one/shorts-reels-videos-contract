75dcf99f90f4029703f9eb14e223e395516ca12a
apptreeso <superwebdev19@gmail.com>
2023-01-13 22:38:07 +0900

    add the function to get the timestamp the name owner updated

--- contracts/interfaces/IVanityURL.sol
+++ contracts/interfaces/IVanityURL.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.17;
 
 interface IVanityURL {
     function setNameOwnerUpdateAt(bytes32 d1dcV2TokenId) external;
+
+    function nameOwnerUpdateAt(bytes32 d1dcV2TokenId) external view returns (uint256);
 }
