beb4c1fcf5b1c2b3d294deb8c830bab2b3f61cf7
Yuriy <yuriy@harmony.one>
2023-02-02 20:24:21 +0300

    Content Manger contract extend

--- contracts/ShortsReelsVideos.sol
+++ contracts/ShortsReelsVideos.sol
@@ -4,31 +4,52 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
-import "./interfaces/IVanityURL.sol";
-
-contract ShortsReelsVideos is
+contract ContentManager is
     OwnableUpgradeable,
-    PausableUpgradeable,
-    ReentrancyGuardUpgradeable
+    PausableUpgradeable
+    // ReentrancyGuardUpgradeable
 {
     /// @dev VanityURL contract address
     IVanityURL public vanityURLAddress;
 
+    struct ContentRecord {
+        address owner;
+        uint256 price;
+    }
+
     /// @dev D1DCV2 Token Id -> Alias Name -> User -> Timestamp the user paid for the video url
     /// @dev Video vanity URL is valid only if nameOwnerUpdateAt <= videoVanityURLPaidAt
-    mapping(bytes32 => mapping(string => mapping(address => uint256))) public videoVanityURLPaidAt;
+    mapping(bytes32 => mapping(string => mapping(address => uint256))) public contentPaidAt;
+
+    mapping(bytes32 => mapping(string => ContentRecord)) public contentRecords;
 
     /// @dev Maintainer address
     address public maintainer;
 
-    event VideoVanityURLPaid(
-        address indexed by,
+    event ContentCreated(
+        address indexed owner,
         string indexed name,
         string indexed aliasNAme,
-        uint256 paidAt
+        uint256 price
     );
+
+    event UserPaidForContent(
+        address indexed owner,
+        string indexed name,
+        string indexed aliasNAme,
+        uint256 price
+    );
+
+    event DonationSent (
+        string indexed name,
+        string indexed aliasNAme,
+        address indexed sender,
+        address recipient,
+        uint256 amount
+    );
+    
     event VanityURLAddressChanged(address indexed from, address indexed to);
     event MaintainerChanged(address indexed from, address indexed to);
 
@@ -61,28 +82,88 @@ contract ShortsReelsVideos is
         maintainer = _maintainer;
     }
 
-    function payForVideoVanityURLAccess(address _user, string memory _name, string memory _aliasName, uint256 _paidAt) external onlyMaintainer {
+    function payForContentAccess(address _user, string memory _name, string memory _aliasName, uint256 _paidAt) public payable {
         bytes32 tokenId = keccak256(bytes(_name));
         require(
-            !checkVideoVanityURLAccess(_user, _name, _aliasName),
+            !checkContentAccess(_user, _name, _aliasName),
             "VanityURL: already paid"
         );
 
         // check the payment timestamp
         require(_paidAt <= block.timestamp, "VanityURL: invalid time");
 
-        // store the payment for the video vanity URL access
-        videoVanityURLPaidAt[tokenId][_aliasName][_user] = _paidAt;
+        require(contentRecords[tokenId][_aliasName].owner != address(0x0), "Content not created");
 
-        emit VideoVanityURLPaid(msg.sender, _name, _aliasName, _paidAt);
+        require(contentRecords[tokenId][_aliasName].price == msg.value, "Wrong payment amount");
+
+        // store the payment for the video vanity URL access
+        contentPaidAt[tokenId][_aliasName][_user] = _paidAt;
+
+        payable(contentRecords[tokenId][_aliasName].owner).transfer(msg.value);
+
+        emit UserPaidForContent(msg.sender, _name, _aliasName, _paidAt);
     }
 
-    function checkVideoVanityURLAccess(address _user, string memory _name, string memory _aliasName) public view returns (bool) {
+    function checkContentAccess(address _user, string memory _name, string memory _aliasName) public view returns (bool) {
         bytes32 tokenId = keccak256(bytes(_name));
-        return
-            vanityURLAddress.nameOwnerUpdateAt(tokenId) <
-                videoVanityURLPaidAt[tokenId][_aliasName][_user]
+
+        return vanityURLAddress.nameOwnerUpdateAt(tokenId) <
+                contentPaidAt[tokenId][_aliasName][_user]
                 ? true
                 : false;
+    }
+
+    function getContentPrice(string memory _name, string memory _aliasName) public view returns (uint256) {
+        bytes32 tokenId = keccak256(bytes(_name));
+
+        return contentRecords[tokenId][_aliasName].price;
+    }
+
+    function createContent(string memory _name, string memory _aliasName, uint256 price) public {
+        string memory ownerName = vanityURLAddress.nameOf(msg.sender);
+        require(compareStrings(ownerName, _name), "Sender not owner of this name");
+
+        return _createContent(msg.sender, _name, _aliasName, price);
+    }
+
+    function forceCreateContent(address _owner, string memory _name, string memory _aliasName, uint256 price) public onlyMaintainer {
+        return _createContent(_owner, _name, _aliasName, price);
+    }
+
+    function _createContent(address _owner, string memory _name, string memory _aliasName, uint256 price) internal {
+        bytes32 tokenId = keccak256(bytes(_name));
+        
+        require(contentRecords[tokenId][_aliasName].owner == address(0x0), "Content alreeady created");        
+        
+        ContentRecord storage content = contentRecords[tokenId][_aliasName];
+
+        content.owner = _owner;
+        content.price = price;
+
+        emit ContentCreated(_owner, _name, _aliasName, price);
+    }
+
+    function sendDonation(string memory _name, string memory _aliasName) public payable {
+        return _sendDonationFrom(msg.sender, _name, _aliasName);
+    }
+
+    function forceSendDonation(address from, string memory _name, string memory _aliasName) public payable onlyMaintainer {
+        return _sendDonationFrom(from, _name, _aliasName);
+    }
+
+    function _sendDonationFrom(address from, string memory _name, string memory _aliasName) internal {
+        bytes32 tokenId = keccak256(bytes(_name));
+        
+        require(contentRecords[tokenId][_aliasName].owner != address(0x0), "Content not created");        
+
+        address recipient = contentRecords[tokenId][_aliasName].owner;
+
+        payable(recipient).transfer(msg.value);
+
+        emit DonationSent(_name, _aliasName, from, recipient, msg.value);
+    }
+
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
 }
--- contracts/interfaces/IVanityURL.sol
+++ contracts/interfaces/IVanityURL.sol
@@ -6,4 +6,6 @@ interface IVanityURL {
     function setNameOwnerUpdateAt(bytes32 d1dcV2TokenId) external;
 
     function nameOwnerUpdateAt(bytes32 d1dcV2TokenId) external view returns (uint256);
+    
+    function nameOf(address addr) external view returns (string memory);
 }
