e3cf257cbf349fbafaad7ef759e777bd8bf48ea9
apptreeso <superwebdev19@gmail.com>
2023-02-07 11:20:27 +0900

    update IVanityURL interface

--- contracts/interfaces/IVanityURL.sol
+++ contracts/interfaces/IVanityURL.sol
@@ -3,9 +3,11 @@
 pragma solidity ^0.8.17;
 
 interface IVanityURL {
-    function setNameOwnerUpdateAt(bytes32 d1dcV2TokenId) external;
-
     function nameOwnerUpdateAt(bytes32 d1dcV2TokenId) external view returns (uint256);
     
-    function nameOf(address addr) external view returns (string memory);
+    function vanityURLPrices(bytes32 didcV2TokenId, string memory aliasName) external view returns (uint256);
+
+    function getNameOwner(string calldata name) external view returns (address);
+
+    function checkURLValidity(string calldata name, string calldata aliasName) external view returns (bool);
 }
