aa74dec23612dcba021d0bb048a1305a04d9303b
apptreeso <superwebdev19@gmail.com>
2023-02-10 11:42:02 +0900

    add the tests for vanity url pricing

--- test/shorts_reels_videos.ts
+++ test/shorts_reels_videos.ts
@@ -8,6 +8,7 @@ import type { MockVanityURL, ShortsReelsVideos } from "../typechain-types"
 
 let dotName = "https://all.1.country/";
 let aliasName = "videos/"
+let ownerRevDisPercent = 6000;  // 60%
 
 const getTimestamp = async (): Promise<BigNumber> => {
   const blockNumber = await ethers.provider.getBlockNumber();
@@ -19,6 +20,7 @@ describe('ShortsReelsVideos', () => {
   let accounts: SignerWithAddress;
   let deployer: SignerWithAddress;
   let alice: SignerWithAddress;
+  let bob: SignerWithAddress;
   let maintainer: SignerWithAddress;
 
   let mockVanityURL: MockVanityURL;
@@ -26,7 +28,7 @@ describe('ShortsReelsVideos', () => {
 
   beforeEach(async () => {
     accounts = await ethers.getSigners();
-    [deployer, alice, maintainer] = accounts;
+    [deployer, alice, bob, maintainer] = accounts;
 
     // Deploy MockVanityURL
     const MockVanityURL = await ethers.getContractFactory("MockVanityURL");
@@ -37,7 +39,7 @@ describe('ShortsReelsVideos', () => {
     shortsReelsVideos = (await upgrades.deployProxy(ShortsReelsVideos, [mockVanityURL.address, maintainer.address])) as ShortsReelsVideos;
 
     // Set the owner revDis percent
-    await shortsReelsVideos.updateOwnerRevDisPercent(6000); // 60%
+    await shortsReelsVideos.updateOwnerRevDisPercent(ownerRevDisPercent);
   });
 
   describe("updateVanityURLAddress", () => {
@@ -50,7 +52,28 @@ describe('ShortsReelsVideos', () => {
     });
 
     it("Should revert if the caller is not owner", async () => {
-      await expect(shortsReelsVideos.connect(alice).vanityURLAddress(alice.address)).to.be.reverted;
+      await expect(shortsReelsVideos.connect(alice).updateVanityURLAddress(alice.address)).to.be.reverted;
+    });
+  });
+
+  describe("updateOwnerRevDisPercent", () => {
+    it("Should be able set the owner revDis percent", async () => {
+      expect(await shortsReelsVideos.owneRevDisPercent()).to.equal(ownerRevDisPercent);
+      
+      const newOwnerRevDisPercent = 5000; // 50%
+      await shortsReelsVideos.updateOwnerRevDisPercent(newOwnerRevDisPercent);
+
+      expect(await shortsReelsVideos.owneRevDisPercent()).to.equal(newOwnerRevDisPercent);
+    });
+
+    it("Should revert if the caller is not owner", async () => {
+      const newOwnerRevDisPercent = 5000; // 50%
+      await expect(shortsReelsVideos.connect(alice).updateOwnerRevDisPercent(newOwnerRevDisPercent)).to.be.reverted;
+    });
+
+    it("Should revert if the percent is greater than 100%", async () => {
+      const newOwnerRevDisPercent = 10001; // 50%
+      await expect(shortsReelsVideos.updateOwnerRevDisPercent(newOwnerRevDisPercent)).to.be.revertedWith("exceeded 100%");
     });
   });
 
@@ -79,12 +102,15 @@ describe('ShortsReelsVideos', () => {
       await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
       await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
 
+      const lockedBalanceBefore = await ethers.provider.getBalance(shortsReelsVideos.address);
       const paymentStatusBefore = await shortsReelsVideos.checkVanityURLAccess(alice.address, dotName, aliasName);
       expect(paymentStatusBefore).to.be.false;
 
       // store the payment status
       await shortsReelsVideos.connect(alice).payForVanityURLAccess(dotName, aliasName, { value: price });
       
+      const lockedBalanceAfter = await ethers.provider.getBalance(shortsReelsVideos.address);
+      expect(lockedBalanceAfter).gt(lockedBalanceBefore);
       const paymentStatusAfter = await shortsReelsVideos.checkVanityURLAccess(alice.address, dotName, aliasName);
       expect(paymentStatusAfter).to.be.true;
     });
@@ -132,38 +158,209 @@ describe('ShortsReelsVideos', () => {
     });
   });
 
-  // describe("payForVanityURLAccess", () => {
-  //   it("Should be able to store the payment status", async () => {
-  //     const paymentStatusBefore = await shortsReelsVideos.checkVanityURLAccess(alice.address, dotName, aliasName);
-  //     expect(paymentStatusBefore).to.be.false;
+  describe("payForVanityURLAccessFor", () => {
+    it("Should be able to store the payment status", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
 
-  //     // store the payment status
-  //     await shortsReelsVideos.connect(maintainer).payForVanityURLAccess(alice.address, dotName, aliasName);
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
 
-  //     const paymentStatusAfter = await shortsReelsVideos.checkVanityURLAccess(alice.address, dotName, aliasName);
-  //     expect(paymentStatusAfter).to.be.true;
-  //   });
+      const lockedBalanceBefore = await ethers.provider.getBalance(shortsReelsVideos.address);
+      const paymentStatusBefore = await shortsReelsVideos.checkVanityURLAccess(alice.address, dotName, aliasName);
+      expect(paymentStatusBefore).to.be.false;
 
-  //   it("Should revert if the payment already stored", async () => {
-  //     const aliasName = "aliasName";
-  //     const paidAt = await getTimestamp();
-  //     await shortsReelsVideos.connect(maintainer).payForVanityURLAccess(alice.address, dotName, aliasName, paidAt);
+      // store the payment status
+      await shortsReelsVideos.connect(maintainer).payForVanityURLAccessFor(alice.address, dotName, aliasName, currentTimestamp, { value: price });
+      
+      const lockedBalanceAfter = await ethers.provider.getBalance(shortsReelsVideos.address);
+      expect(lockedBalanceAfter).gt(lockedBalanceBefore);
+      const paymentStatusAfter = await shortsReelsVideos.checkVanityURLAccess(alice.address, dotName, aliasName);
+      expect(paymentStatusAfter).to.be.true;
+    });
 
-  //     await expect(shortsReelsVideos.connect(maintainer).payForVanityURLAccess(alice.address, dotName, aliasName, paidAt + 1)).to.be.revertedWith("VanityURL: already paid");
-  //   })
+    it("Should revert if the payment was already stored", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
 
-  //   it("Should revert if the caller is not the maintainer", async () => {
-  //     const aliasName = "aliasName";
-  //     const paidAt = await getTimestamp();
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
 
-  //     await expect(shortsReelsVideos.connect(alice).payForVanityURLAccess(alice.address, dotName, aliasName, paidAt)).to.be.revertedWith("VanityURL: only maintainer");
-  //   });
+      // store the payment status
+      await shortsReelsVideos.connect(maintainer).payForVanityURLAccessFor(alice.address, dotName, aliasName, currentTimestamp, { value: price });
 
-  //   it("Should revert if the payment timestamp is invalid", async () => {
-  //     const aliasName = "aliasName";
-  //     const paidAt = await getTimestamp();
+      await expect(shortsReelsVideos.connect(maintainer).payForVanityURLAccessFor(alice.address, dotName, aliasName, currentTimestamp, { value: price })).to.be.revertedWith("already paid");
+    });
 
-  //     await expect(shortsReelsVideos.connect(maintainer).payForVanityURLAccess(alice.address, dotName, aliasName, paidAt + 1)).to.be.revertedWith("VanityURL: invalid time");
-  //   });
-  // });
+    it("Should revert if the vanity URL is invalid", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.add(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      await expect(shortsReelsVideos.connect(maintainer).payForVanityURLAccessFor(alice.address, dotName, aliasName, currentTimestamp, { value: price })).to.be.revertedWith("invalid URL");
+    });
+
+    it("Should revert if the payment amount is wrong", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      await expect(shortsReelsVideos.connect(maintainer).payForVanityURLAccessFor(alice.address, dotName, aliasName, currentTimestamp, { value: price.sub(1) })).to.be.revertedWith("wrong payment amount");
+    });
+
+    it("Should revert if the payment timestamp is greater than the current timestamp", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      await expect(shortsReelsVideos.connect(maintainer).payForVanityURLAccessFor(alice.address, dotName, aliasName, currentTimestamp.add(100), { value: price })).to.be.revertedWith("invalid time");
+    });
+
+    it("Should revert if the caller is not the maintainer", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      await expect(shortsReelsVideos.connect(alice).payForVanityURLAccessFor(alice.address, dotName, aliasName, currentTimestamp, { value: price })).to.be.revertedWith("only maintainer");
+    });
+  });
+
+  describe("sendDonation", () => {
+    it("Should be able to send the donation", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      // donate
+      await mockVanityURL.setNameOwner(bob.address);
+      const nameOwnerBalanceBefore = await ethers.provider.getBalance(bob.address);
+
+      await shortsReelsVideos.connect(alice).sendDonation(dotName, aliasName, { value: price });
+
+      const nameOwnerBalanceAfter = await ethers.provider.getBalance(bob.address);
+      expect(nameOwnerBalanceAfter).gt(nameOwnerBalanceBefore);
+    });
+
+    it("Should revert if the vanity URL is invalid", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.add(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      await expect(shortsReelsVideos.connect(alice).sendDonation(dotName, aliasName, { value: price })).to.be.revertedWith("invalid URL");
+    });
+  });
+
+  describe("sendDonationFor", () => {
+    it("Should be able to send the donation", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      // donate
+      await mockVanityURL.setNameOwner(bob.address);
+      const nameOwnerBalanceBefore = await ethers.provider.getBalance(bob.address);
+
+      await shortsReelsVideos.connect(maintainer).sendDonationFor(alice.address, dotName, aliasName, { value: price });
+
+      const nameOwnerBalanceAfter = await ethers.provider.getBalance(bob.address);
+      expect(nameOwnerBalanceAfter).gt(nameOwnerBalanceBefore);
+    });
+
+    it("Should revert if the vanity URL is invalid", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.add(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      await expect(shortsReelsVideos.connect(maintainer).sendDonationFor(alice.address, dotName, aliasName, { value: price })).to.be.revertedWith("invalid URL");
+    });
+
+    it("Should revert if the caller is not the maintainer", async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      await expect(shortsReelsVideos.connect(alice).sendDonationFor(alice.address, dotName, aliasName, { value: price })).to.be.revertedWith("only maintainer");
+    });
+  });
+
+  describe("withdraw", () => {
+    beforeEach(async () => {
+      // set the vanity URL price
+      const price = ethers.utils.parseEther("1");
+      await mockVanityURL.setVanityURLPrice(dotName, aliasName, price);
+
+      // set the url info
+      const currentTimestamp = await getTimestamp();
+      await mockVanityURL.setNameOwnerUpdateAt(dotName, currentTimestamp.sub(1));
+      await mockVanityURL.setVanityURLUpdateAt(dotName, aliasName, currentTimestamp);
+
+      // store the payment status
+      await shortsReelsVideos.connect(alice).payForVanityURLAccess(dotName, aliasName, { value: price });
+    });
+
+    it("should be able to withdraw ONE tokens", async () => {
+      const ownerBalanceBefore = await ethers.provider.getBalance(deployer.address);
+      
+      // withdraw ONE tokens
+      await shortsReelsVideos.withdraw();
+
+      const ownerBalanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(ownerBalanceAfter).gt(ownerBalanceBefore);
+    });
+
+    it("Should revert if the caller is not the owner", async () => {
+      await expect(shortsReelsVideos.connect(alice).withdraw()).to.be.reverted;
+    });
+  });
 });
